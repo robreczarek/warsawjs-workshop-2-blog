9256d3f25ed42808660e2680d36b30f071110cba
Robert Obreczarek <robert@obreczarek.com>
2017-01-15 16:10:46 +0100

    Updated get method in post.service.js

--- scripts/services/post.service.js
+++ scripts/services/post.service.js
@@ -2,6 +2,7 @@ let posts = [
   {id: 1, title: 'pierwszy post', text: 'tresc posta'},
   {id: 2, title: 'drugi post', text: 'tresc posta drugiego'}
 ]
+
 export class PostService {
 
   static list() {
@@ -20,7 +21,12 @@ export class PostService {
 
   static get(id) {
     return new Promise((resolve, reject) => {
-      resolve(posts[parseInt(id)-1]);
+      for(let post of posts) {
+        if(post.id === parseInt(id)) {
+          return resolve(post);
+        }
+      }
+      reject('Post not found. Please enter a valid post ID.');
     })
   }
 
